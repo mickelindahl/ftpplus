6e0f9c27d021862408a17c17e4b18170ed1f2ee0
Mikael Lindahl <mikael.lindahl@greencargo.com>
2016-09-27 13:17:09 +0200

    test parse crew as promise

--- index.js
+++ index.js
@@ -2,39 +2,89 @@
  * Created by s057wl on 2016-07-14.
  */
 
+'use strict';
+
 const Ftp=require('jsftp');
 const Async=require('async');
+const Promise=require('bluebird');
+const debug=require('debug')('ftpplus');
+
+let _callback;
 
 function fetch(options, callback){
 
-    try {
-        var ftp = new Ftp(options.auth);
-        options.ftp = ftp;
-    }catch(err){
-        console.error('ftp connect failed', err)
-        throw err
-    }
-    try{
-        ftp.list(options.path, function (err, res) {
-            try {
-                if (err) {
-                    throw err
+    _callback=callback;
+
+    return new Promise((resolve,reject)=>{
+
+        try {
+
+            var ftp = new Ftp(options.auth);
+            options.ftp = ftp;
+
+            debug('ftp created')
+
+            resolve(options)
+        }catch(err) {
+
+            reject(err)
+
+        }
+
+    }).then((options)=>{
+
+        return new Promise((resolve, reject)=> {
+            options.ftp.list(options.path, function (err, res) {
+
+                if (err) reject(err);
+
+                try {
+
+                    options.files = parse(res, options);
+
+                    debug('files parsed', options.files)
+
+                    resolve(options);
+
+                } catch (err) {
+
+                    reject(err)
                 }
+            });
+        })
+    }).then((options)=>{
 
-                options.files = parse(res, options);
+        return get(options)
 
-                get(options, callback);
+    }).then((results)=>{
+        if (_callback) _callback(null, results)
+        else return results
+    }).catch((err)=>{
+        if (_callback) _callback(err, null)
+        else throw err
+    });
 
-                // callback(null,files)
-
-            } catch (err) {
-                callback(err)
-            }
-        });
-    }catch(err){
-        console.error('ftp list failed', err)
-        throw err
-    }
+    // try{
+    //     ftp.list(options.path, function (err, res) {
+    //         try {
+    //             if (err) {
+    //                 throw err
+    //             }
+    //
+    //             options.files = parse(res, options);
+    //
+    //             get(options, callback);
+    //
+    //             // callback(null,files)
+    //
+    //         } catch (err) {
+    //             callback(err)
+    //         }
+    //     });
+    // }catch(err){
+    //     console.error('ftp list failed', err)
+    //     throw err
+    // }
 }
 
 function parse(ls, options){
@@ -61,65 +111,147 @@ function parse(ls, options){
         files.push(file);
         i+=1;
     });
+
     return files
 
 }
 
-function get(options, callback){
+function stream(file, options){
 
-    var calls=[];
-    options.files.forEach(function(file){
-        calls.push(function(_callback){
-                if (options.bar != undefined) {
-                    options.bar.setTotal(options.files.length).tick('Fetching: '+ file)
-                };
+    return new Promise((resolve, reject)=>{
 
-                var str = '';
-                // console.debug('path', path)
-                options.ftp.get(options.path+file, function (err, socket) {
+        if (options.bar != undefined) {
+            options.bar.setTotal(options.files.length).tick('Fetching: '+ file)
+        };
 
-                    if (err) {
-                        console.error('ftp get failed', err);
+        var str = '';
+        // console.debug('path', path)
+        options.ftp.get(options.path+file, function (err, socket) {
 
-                        _callback(null, {
-                            text:str,
-                            file:file
-                        });
+            if (err) {
+                return reject( {
+                    text:str,
+                    file:file,
+                    err:err,
+                });
+            };
 
-                        // console.log('closing socket');
-                        // socket.emit('close');
+            socket.on("data", function (buffer) {
 
-                        return
-                    };
+                // binary encoding needed for loken
+                if (options.encoding != undefined) {
+                    str += buffer.toString(options.encoding);
+                } else {
+                    str += buffer.toString();
+                }
+            });
 
-                    socket.on("data", function (buffer) {
-
-                        // binary encoding needed for loken
-                        if (options.encoding != undefined) {
-                            str += buffer.toString(options.encoding);
-                        } else {
-                            str += buffer.toString();
-                        }
+            socket.on("close", function (err) {
+                if (err) {
+                    return reject( {
+                        text:str,
+                        file:file,
+                        err:err,
                     });
 
-                    socket.on("close", function (err) {
-                        if (err) {
-                            console.error('ftp get close failed', err);
-                        }
-                        _callback(null, {
-                            text:str,
-                            file:file
-                        });
-                    });
-                    socket.resume();
-                })
+                }
+
+                str=options.skip && options.skip(str) ? '' : str;
+
+                resolve({
+
+                    text:str,
+                    file:file
+
+                });
+            });
+            socket.resume();
         })
+    }).catch((result)=>{
+
+        console.error(result.err);
+        return result
+    })
+}
+
+function get(options, done){
+
+
+    // start with current being an "empty" already-fulfilled promise
+    var current = Promise.resolve();
+
+    return Promise.all(options.files.map((file)=> {
+        current = current.then(function() {
+            return stream(file, options)
+        });
+        return current;
+    })).then((resolved)=> {
+        return options.post_process
+            ? options.post_process(resolved)
+            : resolved
+
+    }).then((resolved)=> {
+
+        if (done) done(resolved);
+        else return resolved;
+
+        // results is an array of names
     });
 
-    Async.series(calls,
-        function(err,results){
-        callback(err, results);
-    })
+
+    // var calls=[];
+    // options.files.forEach(function(file){
+    //     calls.push(function(_callback){
+    //         if (options.bar != undefined) {
+    //             options.bar.setTotal(options.files.length).tick('Fetching: '+ file)
+    //         };
+    //
+    //         var str = '';
+    //         // console.debug('path', path)
+    //         options.ftp.get(options.path+file, function (err, socket) {
+    //
+    //             if (err) {
+    //                 console.error('ftp get failed', err);
+    //
+    //                 _callback(null, {
+    //                     text:str,
+    //                     file:file
+    //                 });
+    //
+    //                 // console.log('closing socket');
+    //                 // socket.emit('close');
+    //
+    //                 return
+    //             };
+    //
+    //             socket.on("data", function (buffer) {
+    //
+    //                 // binary encoding needed for loken
+    //                 if (options.encoding != undefined) {
+    //                     str += buffer.toString(options.encoding);
+    //                 } else {
+    //                     str += buffer.toString();
+    //                 }
+    //             });
+    //
+    //             socket.on("close", function (err) {
+    //                 if (err) {
+    //                     console.error('ftp get close failed', err);
+    //                 }
+    //                 _callback(null, {
+    //                     text:str,
+    //                     file:file
+    //                 });
+    //             });
+    //             socket.resume();
+    //         })
+    //     })
+    // });
+    //
+    // Async.series(calls,
+    //     function(err,results){
+    //         callback(err, results);
+    //     })
 }
 
 module.exports={
--- test/index.js
+++ test/index.js
@@ -5,8 +5,10 @@
 
 const Code = require('code');   // assertion library
 const Lab=require('lab');
-const Path=require('path')
+const Path=require('path');
 const Ftp = require('../index');
+const debug=require('debug')('ftpplus:test');
+const Parse=require('parse');
 
 var lab = exports.lab = Lab.script();
 var root = Path.resolve();
@@ -25,74 +27,76 @@ lab.experiment('parse', function () {
         done();
     });
 
-    lab.test('fetch pers', function (done) {
+    lab.test('fetch pers with parse', function (done) {
         var options={
             auth:auth,
             path:process.env.FTP_PATH_PERS,
-            encoding:'binary'
+            encoding:'binary',
+            post_process:Parse.crews
         };
 
-        Ftp.fetch(options, function(err, results){
-            if (err) {
-                return console.error(err);
-            }
+        Ftp.fetch(options).then((results)=>{
 
             Code.expect(results).to.be.an.array();
             done();
-        });
+
+        }).catch((err)=>{
+            console.error(err);
+            done()
+        })
     });
 
-    lab.test('fetch loken', function (done) {
-        var options={
-            auth:auth,
-            path:process.env.FTP_PATH_LOKEN,
-            encoding:'binary'
-        };
-
-        Ftp.fetch(options, function(err, results){
-            if (err) {
-                return console.log(err);
-            }
-            Code.expect(results).to.be.an.array();
-            done();
-        });
-    });
-
-    lab.test('fetch train running', function (done) {
-
-        var options={
-            auth:auth,
-            path:process.env.FTP_PATH_TRRU,
-            filter: [
-                'TrainRunningInformation_20160420-083401-524.xml',
-                'TrainRunningInformation_20160420-083404-810.xml']
-        };
-
-        Ftp.fetch(options, function(err, results){
-            if (err) {
-                return console.log(err);
-            }
-            Code.expect(results).to.be.an.array();
-            Code.expect(results.length).to.equal(77);
-            done();
-        });
-    });
-
-    lab.test('fetch utin', function (done) {
-
-        var options={
-            auth:auth,
-            path:process.env.FTP_PATH_UTIN,
-            limit:1
-        };
-
-        Ftp.fetch(options, function(err, results){
-            if (err) {
-                return console.log(err);
-            }
-            Code.expect(results).to.be.an.array();
-            done();
-        });
-    });
+    // lab.test('fetch loken', function (done) {
+    //     var options={
+    //         auth:auth,
+    //         path:process.env.FTP_PATH_LOKEN,
+    //         encoding:'binary'
+    //     };
+    //
+    //     Ftp.fetch(options, function(err, results){
+    //         if (err) {
+    //             return console.log(err);
+    //         }
+    //         Code.expect(results).to.be.an.array();
+    //         done();
+    //     });
+    // });
+    //
+    // lab.test('fetch train running', function (done) {
+    //
+    //     var options={
+    //         auth:auth,
+    //         path:process.env.FTP_PATH_TRRU,
+    //         filter: [
+    //             'TrainRunningInformation_20160420-083401-524.xml',
+    //             'TrainRunningInformation_20160420-083404-810.xml']
+    //     };
+    //
+    //     Ftp.fetch(options, function(err, results){
+    //         if (err) {
+    //             return console.log(err);
+    //         }
+    //         Code.expect(results).to.be.an.array();
+    //         Code.expect(results.length).to.equal(77);
+    //         done();
+    //     });
+    // });
+    //
+    // lab.test('fetch utin', function (done) {
+    //
+    //     var options={
+    //         auth:auth,
+    //         path:process.env.FTP_PATH_UTIN,
+    //         limit:1
+    //     };
+    //
+    //     Ftp.fetch(options, function(err, results){
+    //         if (err) {
+    //             return console.log(err);
+    //         }
+    //         Code.expect(results).to.be.an.array();
+    //         done();
+    //     });
+    // });
 
 });
