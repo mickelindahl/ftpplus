3d973347596c40612bff1b18ab867be9b0cc0c0d
Mikael Lindahl <mikael.lindahl@greencargo.com>
2017-01-16 12:23:42 +0000

    added to package.json

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const Promise = require( 'bluebird' );
 const Path = require( 'path' )
 const parse = require('parse')
 
-let credentials = {
-    host: process.env.HOST
-    , user: process.env.USER
-    , password: process.env.PASS
-}
 
 function diskList() {
 }
